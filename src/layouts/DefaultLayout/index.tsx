f11bbfc115ec47b464f87575339a9612d1f4ba7c
marettialine <alinesmaretti@gmail.com>
2024-07-10 22:07:37 -0300

    Update default layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
 
-import { Container, LayoutContainer } from './styles'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
